016e647734c081a917e2de872d4166b0621e6f96
Syafiq-Standable <64371908+Syafiq-Standable@users.noreply.github.com>
2025-12-11 19:35:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -485,7 +485,7 @@ wa.me/6289528950624
                 const from = msg.key.remoteJid;
                 const text = (msg.message?.conversation || msg.message?.extendedTextMessage?.text || msg.message?.imageMessage?.caption || '').trim();
 
-                    const msg = m.messages[0];
+                    
 if (!msg.message || msg.key.fromMe) return;
 
 // ===============================
